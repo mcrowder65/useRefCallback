0b099469f2cfcd9bceca5a625a4953df044cf7fd
Matt Crowder <mcrowder65@gmail.com>
2022-01-02 15:43:34 -0600

    remove providers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "src/app"
-import Providers from "src/providers"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById("root"),
 )
