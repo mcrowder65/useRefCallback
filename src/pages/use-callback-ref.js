b7b6e5beba18727ceb1cdb855381c7a3cf8ba579
Matt Crowder <mcrowder65@gmail.com>
2022-01-05 19:32:35 -0600

    add use-callback-ref

---
+++ src/pages/use-callback-ref.js
@@ -0,0 +1,33 @@
+import React from "react"
+const useCallbackRef = (callback) => {
+  const callbackRef = React.useRef(null)
+
+  React.useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+  return callbackRef
+}
+const useEventListener = (type, callback) => {
+  const callbackRef = useCallbackRef(callback)
+  React.useEffect(() => {
+    let callback = callbackRef.current
+    window.addEventListener(type, callback)
+    return () => {
+      window.removeEventListener(type, callback)
+    }
+  }, [type])
+}
+function UseCallbackRef() {
+  const [count, setCount] = React.useState(0)
+  useEventListener("resize", () => {
+    setCount((prev) => prev + 1)
+  })
+  return (
+    <div>
+      <p>I only subscribe once by the power of refs!</p>
+      <p>Count: {count}</p>
+    </div>
+  )
+}
+
+export default UseCallbackRef
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom"
 import Simple from "src/pages/simple"
 import ExternalExample from "src/pages/external-example"
 import CallbackRef from "src/pages/callback-ref"
+import UseCallbackRef from "src/pages/use-callback-ref"
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
       <Route path="/simple" component={Simple} exact />
       <Route path="/external" component={ExternalExample} exact />
       <Route path="/callback-ref" component={CallbackRef} exact />
+      <Route path="/use-callback-ref" component={UseCallbackRef} exact />
     </Switch>
   )
 }
